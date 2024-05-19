5b62367adf93bf3970e77bc4c9f1bcaa1cc96253
Martyna <m.michalska4126@gmail.com>
2024-05-19 17:29:39 +0200

    feat: first layout components for dashboard

--- screens/app/Dashboard.tsx
+++ screens/app/Dashboard.tsx
@@ -7,10 +7,74 @@ type DashboardProps = BottomTabScreenProps<AppTabParamList, 'Dashboard'>;
 
 const DashboardScreen: React.FC<DashboardProps> = ({navigation}) => {
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Text>Main Dashboard</Text>
+    <View style={{flex: 1, marginTop: 80}}>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 10,
+        }}
+      >
+        <SummaryView amount="2220" name="Summary" color="pink" size="big" />
+        <View style={{gap: 10}}>
+          <SummaryView
+            amount="220"
+            name="Receivable"
+            color="green"
+            size="small"
+          />
+          <SummaryView amount="700" name="Debt" color="red" size="small" />
+        </View>
+      </View>
     </View>
   );
 };
 
 export default DashboardScreen;
+
+const SummaryView = ({
+  amount,
+  name,
+  color,
+  size,
+}: {
+  amount: string;
+  name: string;
+  color: string;
+  size: 'small' | 'big';
+}) => {
+  return (
+    <View
+      style={{
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <View
+        style={{
+          borderRadius: 100,
+          width: size === 'small' ? 100 : 150,
+          height: size === 'small' ? 100 : 150,
+          backgroundColor: color,
+          alignItems: 'center',
+          justifyContent: 'center',
+          overflow: 'hidden',
+        }}
+      >
+        <Text
+          style={{
+            fontSize: size === 'small' ? 16 : 20,
+            fontWeight: 500,
+            paddingHorizontal: 10,
+          }}
+          numberOfLines={1}
+        >
+          {amount} zł
+        </Text>
+      </View>
+      <Text style={{fontSize: size === 'small' ? 12 : 14}}>{name}</Text>
+    </View>
+  );
+};
