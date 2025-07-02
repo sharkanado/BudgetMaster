8a492d77b1c9c4914d447edb5abdea1352681247
Martyna <m.michalska4126@gmail.com>
2025-07-02 13:26:26 +0200

    fix: attributes cleanup

--- app/src/main/java/com/example/budgetmaster/MainActivity.kt
+++ app/src/main/java/com/example/budgetmaster/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.budgetmaster
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
